0757a2e9a75fd7b7a0074326d0d5a914d2d884ff
MacFan4000 <paulfkeffer@gmail.com>
2017-07-14 07:58:32 -0400

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -119,7 +119,7 @@ $wgDefaultSkin = "refreshed";
 # The following skins were automatically enabled:
 wfLoadSkin( 'CologneBlue' );
 wfLoadSkin( 'Modern' );
-wfLoadSkin( 'Monobook' );
+wfLoadSkin( 'MonoBook' );
 wfLoadSkin( 'Vector' );
 wfLoadSkin( 'Refreshed' );
 
